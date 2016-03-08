baeb42cc7e25419cf31f2dfc160caa0042bed0d6
alizauf <aliza.aufrichtig@gmail.com>
2016-03-08 14:07:20 -0500

    Popup content and styles

--- popup.js
+++ popup.js
@@ -1,20 +1,26 @@
-window.onload = popupLogic;
+window.onload = popupLogic
+var textAddress = null
 
-function popupLogic() {
-	var address = chrome.extension.getBackgroundPage().address;
-	address.map(function(obj) {
-		// date cleaning
-		if (obj.closed_date) {
-			obj.closed_date = new Date(obj.closed_date)
-				.toDateString().substring(4);
-		}
+function popupLogic () {
+  var address = chrome.extension.getBackgroundPage().address
+  address.map(function (obj) {
+    // date cleaning
+    if (obj.closed_date) {
+      obj.closed_date = new Date(obj.closed_date)
+        .toDateString().substring(4)
+    }
 
-		obj.created_date = new Date(obj.created_date)
-			.toDateString().substring(4);
+    obj.created_date = new Date(obj.created_date)
+      .toDateString().substring(4)
 
-		$("#complaint-list").append('<li>' + obj.created_date + '\n' + obj.closed_date +
-					'\n' + obj.resolution_description + '\n' + obj.descriptor + '</li>');
-	});
+    textAddress = obj.incident_address
 
+    $('.complaintList').append('<div class="complaintItem"><h2>' + obj.created_date + '</h2> <div class="moreInfo"> <p><span class="category">Incident Description:</span> ' + obj.descriptor + '</p><p><span class="category">Category:</span> ' + obj.complaint_type + '</p><p><span class="category">Close date:</span> ' + obj.closed_date + '</p><p><span class="category">Resolution description:</span> ' + obj.resolution_description + '</p></div></div>')
+
+  })
+
+  $('#headline').append('<h1> Historical 311 Complaints from ' + textAddress + '</h1>')
+
+  $('complaint')
 
 }
