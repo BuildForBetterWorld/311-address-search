e6faa1b7224c5d75afed666ac68016e7bf2a1e29
alizauf <aliza.aufrichtig@gmail.com>
2016-03-08 16:50:52 -0500

    311 link opens

--- content.js
+++ content.js
@@ -4,81 +4,80 @@
 // trulia: <span itemprop="streetAddress"> 
 // zillow: <header class="addr"> 
 
-find311Data();
+find311Data()
 
-function cleanTruliaAddress() {
-	var rawAddress = document.getElementsByClassName('h2 headingDoubleSuper')[0].textContent;
+function cleanTruliaAddress () {
+  var rawAddress = document.getElementsByClassName('h2 headingDoubleSuper')[0].textContent
 
 }
 
-function cleanStreetEasyAddress() {
-	// obtain raw address 
-	var rawAddress = document.getElementsByTagName('h1')[0].textContent;
+function cleanStreetEasyAddress () {
+  // obtain raw address 
+  var rawAddress = document.getElementsByTagName('h1')[0].textContent
 
-	// This section splits any address into an array
-	  var regexNoApt = /\s(\S.*)\s#/
-	  var addressNoApt = regexNoApt.exec(rawAddress)[1]
-	  var addressArray = addressNoApt.split(' ')
+  // This section splits any address into an array
+  var regexNoApt = /\s(\S.*)\s#/
+  var addressNoApt = regexNoApt.exec(rawAddress)[1]
+  var addressArray = addressNoApt.split(' ')
 
-	  // This section fixes the number
+  // This section fixes the number
 
-	  var ordinalDict = {'w': 'west', 'w.': 'west', 'e': 'east', 'e.': 'east', 'n': 'north', 'n.': 'north', 's': 'south', 's.': 'south'}
-	  var regexRemoveNd = /(\d+)(st|nd|rd|th)/
-	  var cleanAddressArray = []
+  var ordinalDict = {'w': 'west', 'w.': 'west', 'e': 'east', 'e.': 'east', 'n': 'north', 'n.': 'north', 's': 'south', 's.': 'south'}
+  var regexRemoveNd = /(\d+)(st|nd|rd|th)/
+  var cleanAddressArray = []
 
-	  function isDirection (item) {
-	    if (item.toLowerCase() in ordinalDict) {
-	      return ordinalDict[item.toLowerCase()]
-	    } else {
-	      return item
-	    }
-	  }
+  function isDirection (item) {
+    if (item.toLowerCase() in ordinalDict) {
+      return ordinalDict[item.toLowerCase()]
+    } else {
+      return item
+    }
+  }
 
-	  function isNumberedStreet (item) {
-	    if (regexRemoveNd.exec(item)) {
-	      return regexRemoveNd.exec(item)[1]
-	    } else {
-	      return item
-	    }
-	  }
+  function isNumberedStreet (item) {
+    if (regexRemoveNd.exec(item)) {
+      return regexRemoveNd.exec(item)[1]
+    } else {
+      return item
+    }
+  }
 
-	  for (i = 0; i < addressArray.length; i++) {
-	    cleanAddressArray[i] = isNumberedStreet(isDirection(addressArray[i]))
-	  }
+  for (i = 0; i < addressArray.length; i++) {
+    cleanAddressArray[i] = isNumberedStreet(isDirection(addressArray[i]))
+  }
 
-	  // returns the urlArray to put into the API request
-	  var urlBase = cleanAddressArray.join('%20')
-	  var url = 'https://data.cityofnewyork.us/resource/erm2-nwe9.json?$select=incident_address,complaint_type,descriptor,resolution_description,created_date,closed_date,incident_zip&incident_address=%27' + urlBase + '%27'
-	  return url
+  // returns the urlArray to put into the API request
+  var urlBase = cleanAddressArray.join('%20')
+  var url = 'https://data.cityofnewyork.us/resource/erm2-nwe9.json?$select=incident_address,complaint_type,descriptor,resolution_description,created_date,closed_date,incident_zip&incident_address=%27' + urlBase + '%27'
+  return url
 }
 
-function find311Data() {
-	// identify website and compute api url
-	var site = document.URL;
-	if (site.includes("trulia")) {
-		var url = cleanTruliaAddress();
-	} else if (site.includes("streeteasy")) {
-		var url = cleanStreetEasyAddress();
-	} else if (site.includes("zumper")) {
-		var url = cleanZumperAddress();
-	} else if (site.includes("zillow")) {
-		var url = cleanZillowAddress();
-	} else {
-		var url = '';
-	}
-	
-	var response = {};
-	$.ajax({
-		url: url,
-		crossDomain: true,
-		success: function(res) {
-			chrome.runtime.sendMessage(res);
-			//console.log("sent res: ", res);
-		},
-		error: function(res) {
-			chrome.runtime.sendMessage(res);
-			//console.log("sent err: ", res);
-		}
-	});
-}
+function find311Data () {
+  // identify website and compute api url
+  var site = document.URL
+  if (site.includes('trulia')) {
+    var url = cleanTruliaAddress()
+  } else if (site.includes('streeteasy')) {
+    var url = cleanStreetEasyAddress()
+  } else if (site.includes('zumper')) {
+    var url = cleanZumperAddress()
+  } else if (site.includes('zillow')) {
+    var url = cleanZillowAddress()
+  } else {
+    var url = ''
+  }
 
+  var response = {}
+  $.ajax({
+    url: url,
+    crossDomain: true,
+    success: function (res) {
+      chrome.runtime.sendMessage(res)
+    // console.log("sent res: ", res)
+    },
+    error: function (res) {
+      chrome.runtime.sendMessage(res)
+    // console.log("sent err: ", res)
+    }
+  })
+}
