0a903012b9e1de34468c0afd3754140a9aff7727
Hari Mohanraj <mohanraj.hari@gmail.com>
2016-03-08 12:49:01 -0500

    icons

--- background.js
+++ background.js
@@ -5,7 +5,9 @@ chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
   	if (request !== {}) {
   		address = request;
-  		chrome.browserAction.setIcon({path: "red.png"});
+  		chrome.browserAction.setIcon({path: "icon16-red.png",
+  									  tabId: sender.tab.tabId
+  									 });
   	} 
 });
 
